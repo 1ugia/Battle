a675cfee91b66506508823e8e0566d444fec6671
Masako Gray <masakogray@hotmail.co.uk>
2021-06-11 19:41:21 +0100

    refactoring name method - getting ready to attack

--- app.rb
+++ app.rb
@@ -16,20 +16,27 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do 
-    session[:player1_name] = params[:player1_name]
-    session[:player2_name] = params[:player2_name]
+    # session[:player1_name] = params[:player1_name]
+    # session[:player2_name] = params[:player2_name]
+    # created new Play class to input name under $player.
+    $player1 = Player.new(params[:player1_name])
+    $player2 = Player.new(params[:player2_name])
     redirect'/play'
   end # 'redirect' will forward to info to the page you want the info to reach
 
   get '/play' do
-    @player1_name = session[:player1_name]
-    @player2_name = session[:player2_name]
+    # @player1_name = session[:player1_name]
+    # @player2_name = session[:player2_name]
+    @player1_name = $player1.name
+    @player2_name = $player2.name
     erb(:play)
   end
 
   get '/attack' do
-    @player1_name = session[:player1_name]
-    @player2_name = session[:player2_name]
+    # @player1_name = session[:player1_name]
+    # @player2_name = session[:player2_name]
+    @player1_name = $player1.name
+    @player2_name = $player2.name
     erb(:attack)
   end
 
