f70324d96b79ae7d4692ea24dc3e39a7fa0450fb
Masako Gray <masakogray@hotmail.co.uk>
2021-06-11 17:28:17 +0100

    DRY out to sign in and play method

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,8 @@
 feature 'View hit points' do
+
   scenario 'See Player 2 hit points' do
-    visit('/')
-    fill_in :player1_name, with: 'Charlotte'
-    fill_in :player2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Mittens: 60HP'
   end
+  
 end
